bce2e82103e799bf6e8bf5c07b740a9409a00be7
hiroshi <gonzalesromanhiroshiyaisen@gmail.com>
2022-12-06 14:40:49 -0500

    Quinto Commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,39 +5,35 @@ class HomePage extends StatelessWidget {
   CollectionReference tasksReference =
       FirebaseFirestore.instance.collection('tasks');
 
-  Stream<int> counter() async*{
-    for (int i = 0; i < 10; i++) {
-      yield i;
-      await Future.delayed(const Duration(seconds: 2));
-    }
-  }
-
-  Future<int> getNumber() async {
-    return 1000;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Firedbase Firestore"),
-        ),
-        body: StreamBuilder(
-          stream: counter(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              int data = snapshot.data;
-              return Center(
-                child: Text(
-                  data.toString(),
-                  style: TextStyle(fontSize: 50),
-                ),
-              );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ));
+      appBar: AppBar(
+        title: Text("Firedbase Firestore"),
+      ),
+      body: StreamBuilder(
+        stream: tasksReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.hasData) {
+            QuerySnapshot collection = snap.data;
+            List<QueryDocumentSnapshot> docs = collection.docs;
+            List<Map<String, dynamic>> docsMap =
+                docs.map((e) => e.data() as Map<String, dynamic>).toList();
+            print(docsMap);
+            return ListView.builder(
+                itemCount: docsMap.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(docsMap[index]["title"]),
+                  );
+                }
+                );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 }
