e2b1f70e04c3584a85f942ecd91c5a5e5c866360
hiroshi <gonzalesromanhiroshiyaisen@gmail.com>
2022-12-12 14:18:56 -0500

    Sexto commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tasks/ui/general/colors.dart';
+import 'package:tasks/ui/widgets/general_widgets.dart';
+import 'package:tasks/ui/widgets/textfield_search_widget.dart';
 
 class HomePage extends StatelessWidget {
   CollectionReference tasksReference =
@@ -8,10 +11,160 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Firedbase Firestore"),
+      backgroundColor: KBrandSecondaryColor,
+      floatingActionButton: InkWell(
+        onTap: () {
+
+        },
+        borderRadius: BorderRadius.circular(14.0),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8.0),
+          decoration: BoxDecoration(
+            color: KBrandPrimaryColor,
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              Text(
+                "Nueva tarea",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: StreamBuilder(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 22.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 12,
+                      offset: const Offset(4, 4),
+                    ),
+                  ]),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Bienvenido, Ethan",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: KBrandPrimaryColor,
+                      ),
+                    ),
+                    Text(
+                      "Mis tareas",
+                      style: TextStyle(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.w600,
+                        color: KBrandPrimaryColor,
+                      ),
+                    ),
+                    divider10(),
+                    TextFieldSearchWidget(),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    " Todas mis tareas",
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600,
+                      color: KBrandPrimaryColor.withOpacity(0.85),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0, vertical: 16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(14.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.04),
+                          offset: const Offset(4, 4),
+                          blurRadius: 12.0,
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 2.5),
+                          decoration: BoxDecoration(
+                              color: Color(0xff3E80FF),
+                              borderRadius: BorderRadius.circular(6.0)),
+                          child: Text(
+                            "Personal",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ),
+                        divider3(),
+                        Text(
+                          "Lorem ipsum dolor sit amet",
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w600,
+                              color: KBrandPrimaryColor.withOpacity(0.85)),
+                        ),
+                        Text(
+                          "Lorem Lorem ipsum dolor sit Lorem ipsum dolor sit amet dolor sit amet",
+                          style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500,
+                              color: KBrandPrimaryColor.withOpacity(0.75)),
+                        ),
+                        divider6(),
+                        Text(
+                          "10/12/2022",
+                          style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500,
+                              color: KBrandPrimaryColor.withOpacity(0.75)),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              ),
+
+
+          ],
+        ),
+      ),
+      /*body: StreamBuilder(
         stream: tasksReference.snapshots(),
         builder: (BuildContext context, AsyncSnapshot snap) {
           if (snap.hasData) {
@@ -33,7 +186,7 @@ class HomePage extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         },
-      ),
+      ),*/
     );
   }
 }
---
+++ lib/ui/general/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+import 'package:flutter/animation.dart';
+
+Color KBrandPrimaryColor = const Color(0xff2C3550);
+Color KBrandSecondaryColor = const Color(0xffF4F6FF);
