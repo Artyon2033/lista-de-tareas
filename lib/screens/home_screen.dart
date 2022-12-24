1ae9fc991c4ea7cd0ae158a5f53f5f27ccfb8dde
Artyon2033 <201913193h@utea.edu.pe>
2022-12-24 17:50:16 -0500

    Segundo commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tasks/screens/note_editor.dart';
+import 'package:tasks/screens/note_reader.dart';
 import 'package:tasks/style/app_style.dart';
-
+import 'package:tasks/widgets/note_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -39,10 +41,11 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 20.0,
             ),
-            StreamBuilder<QuerySnapshot>(
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
                 stream:
                     FirebaseFirestore.instance.collection("Notes").snapshots(),
-                builder: (context, AsyncSnapshot snapshot) {
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                   //Aqui revisamos la coneccion con firebase
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Center(
@@ -51,16 +54,37 @@ class _HomeScreenState extends State<HomeScreen> {
                   }
                   if (snapshot.hasData) {
                     return GridView(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2));
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      children: snapshot.data!.docs
+                          .map((note) => noteCard(() {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          NoteReaderScreen(note),
+                                    ));
+                              }, note))
+                          .toList(),
+                    );
                   }
-                  return Text("ther's no Notes",style: GoogleFonts.nunito(color:Colors.white),);
+                  return Text(
+                    "ther's no Notes",
+                    style: GoogleFonts.nunito(color: Colors.white),
+                  );
                 },
-                
-                )
-
+              ),
+            )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => NoteEditorScreen()));
+        },
+        label: Text("Add Note"),
+        icon: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/widgets/note_card.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:tasks/style/app_style.dart';
+
+Widget noteCard(Function()? onTap, QueryDocumentSnapshot doc) {
+  return InkWell(
+      onTap: onTap,
+      child:
+          Container(
+            padding: EdgeInsets.all(8.0),
+             margin: EdgeInsets.all(8.0),
+             decoration: BoxDecoration(
+              color: AppStyle.cardsColor[doc['color_id'] ],
+              borderRadius: BorderRadius.circular(8.0),
+             ),
+             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(doc["Titulo_Nota"],
+                style: AppStyle.mainTitle,
+                ),
+                SizedBox(height: 4.0,), 
+                Text(doc["Fecha de creacion"],
+                style: AppStyle.dateTitle,
+                ),
+                SizedBox(height: 4.0,), 
+                Text(doc["Contenido de la Nota"],
+                style: AppStyle.mainContent,
+                overflow: TextOverflow.ellipsis,
+                ),
+              ],
+             ),
+             )
+             );
+}
